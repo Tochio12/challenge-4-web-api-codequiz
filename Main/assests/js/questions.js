dea54f284b6c8c14d10cece8d120976ae2d73388
Melany Baldeon <tochio12910@gmail.com>
2022-11-07 00:11:28 -0500

    added a new script file for the questions added some logic for it could go through the questions

---
+++ Main/assests/js/questions.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        question: "How?",
+        answers: [ 
+            { text: "A", correct: false },
+            { text: "B", correct: false },
+            { text: "C", correct: false },
+            { text: "D", correct: true }
+        ]
+    },
+    {
+        question: "Why?",
+        answers: [ 
+            { text: "A", correct: false },
+            { text: "B", correct: false },
+            { text: "C", correct: false },
+            { text: "D", correct: true }
+        ]
+    },
+]
---
+++ Main/assests/js/script.js
@@ -0,0 +1,59 @@
+var timeLeft = 75;
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-button");
+var startSectionEl = document.getElementById("start-section");
+var questionSectionEl = document.getElementById("question-section");
+var nextButton = document.getElementById("next-button");
+var currentQuestion;
+var questionEl = document.getElementById("questions");
+var answerButtonEl = document.getElementById("answer-buttons");
+var shuffledQuestions;
+
+
+//getting the start buuton to work and display the next button
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+    currentQuestion++
+    nextQuestion()
+})
+
+// timer when the game starts
+function countdown() {
+    timeLeft--;
+    timerEl.textContent="Time: " + timeLeft;
+    if (timeLeft <= 0) {
+
+    }
+}
+
+// start game
+function startQuiz() {
+    timerID = setInterval(countdown, 1000);
+    startSectionEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestion = 0
+    questionSectionEl.classList.remove("hide");
+    countdown();
+    nextQuestion();
+
+}
+
+// next question
+function nextQuestion() {
+    displayQuestion(shuffledQuestions[currentQuestion]);
+}
+
+// show question
+function displayQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => 
+        {
+            var button = document.createElement("button")
+            button.innerText = answer.text
+            button.classList.add("btn")
+            if(answer.correct) {
+                button.dataset.correct = answer.correct
+            }
+        })
+}
+
